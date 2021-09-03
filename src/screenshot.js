c443e38d470c3a4a29a6c52091aff22ceacb0a7f
Roberto Achar <robertoachar@gmail.com>
2021-09-03 08:27:07 -0300

    feature: check ungraber

--- src/screenshot.js
+++ src/screenshot.js
@@ -1,7 +1,14 @@
 import puppeteer from 'puppeteer';
 
 const start = async (url) => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+    ],
+    headless: true,
+  });
   console.log(`Browser is connected: ${browser.isConnected()}`);
 
   const page = await browser.newPage();
@@ -11,14 +18,26 @@ const start = async (url) => {
     deviceScaleFactor: 2,
   });
 
+  await page.setRequestInterception(true);
+  page.on('request', (request) => {
+    if (request.url().includes('ungrabber')) {
+      console.log(request.url());
+      request.abort();
+    } else {
+      request.continue();
+    }
+  });
+
   console.log(`Loading ${url}`);
   await page.goto(url, {
-    timeout: 60000,
+    timeout: 30000,
     waitUntil: 'networkidle0',
   });
 
+  await page.waitFor(5 * 1000);
+
   console.log(`Taking screenshot...`);
-  const screenshot = await page.screenshot();
+  const screenshot = await page.screenshot({ fullPage: true });
 
   await browser.close();
   console.log(`Browser is connected: ${browser.isConnected()}`);
